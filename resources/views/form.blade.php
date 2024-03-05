28fba379f4da86a2b11042f12105a76a351efbb0
Hoai <hoai.nguyen25@student.passerellesnumeriques.org>
2024-03-05 20:44:38 +0700

    Request

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,22 +1,9 @@
 <form action="{{ url('/post') }}" method="POST">
     @csrf
-    
-    <p>Name</p>
-    <div>
-        <input type="text" name="name" value="">
-    </div>
 
-    <p>UserName</p>
-    <div>
-        <input type="text" name="username" value="">
-    </div>
-
-    <p>Password</p>
-    <div class="">
-        <input type="password" name="password">
-    </div>
-    <br>
-
+    <div> <p>Name: <input type="text" name="name" value=""></p> </div>
+    <div> <p>UserName: <input type="text" name="username" value=""></p> </div>
+    <div> <p>Password: <input type="password" name="password"></p> </div>
     <div>
         <button type="submit">Register</button>
     </div>
